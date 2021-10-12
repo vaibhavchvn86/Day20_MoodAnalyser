28423c14a90f52313f89324a2f77a0d6519b4dda
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-12 22:41:27 +0530

    [VaibhavChavan]Added:Handel Exception in Mood Analyser Objects

---
+++ UC2_MoodAnalyser/UC2_MoodAnalyser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MoodAnalyserAppWithCore
+{
+    public class UC2_MoodAnalyser
+    {
+        private string message;
+        public UC2_MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public UC2_MoodAnalyser()
+        {
+        }
+        public string analyseMood()
+        {
+            //"null"==""
+            // string s = null;
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("Sad"))
+                    return "Sad";
+                else
+                    return "Happy";
+            }
+            catch (NullReferenceException e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
